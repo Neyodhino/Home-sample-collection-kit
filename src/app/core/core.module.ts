4aac60f9db85b309c58543eec767a28c0cecc0ef
Olaniyi Awofisayo <olaniyiawofisayo@gmail.com>
2020-05-26 14:42:04 +0100

    Project base setup

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,9 @@
+import {NgModule} from '@angular/core';
+
+import {NavbarComponent} from './navbar/navbar.cmponent';
+
+@NgModule({
+  declarations: [NavbarComponent],
+  exports: [NavbarComponent],
+})
+export class CoreModule {}
