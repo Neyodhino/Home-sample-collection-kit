fbe6bd03785ac46f302334253aebbc580cc729ed
Olaniyi Awofisayo <olaniyiawofisayo@gmail.com>
2020-05-27 03:09:30 +0100

    Navabar implemented

---
+++ src/app/landingPage/landingPage.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line: component-selector
+  selector: 'landingPage',
+  templateUrl: './landingPage.component.html',
+  styleUrls: ['./landingPage.component.scss']
+})
+export class LandingPageComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void { }
+}
