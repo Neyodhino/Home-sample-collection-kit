2e114ca6b429b7b2685f57888f4c9d342eccc6c4
Olaniyi Awofisayo <olaniyiawofisayo@gmail.com>
2020-05-28 03:01:06 +0100

    audio player added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {CoreModule} from './core/core.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +16,7 @@ import { LandingPageComponent } from './landingPage/landingPage.component';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/landingPage/landingPage.component.ts
+++ src/app/landingPage/landingPage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   // tslint:disable-next-line: component-selector
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landingPage.component.scss']
 })
 export class LandingPageComponent implements OnInit {
+  heart = faHeart;
   constructor() { }
 
   ngOnInit(): void { }
+  downloadFile(){
+    const link = document.createElement('a');
+    link.download = 'how-to-use';
+    link.href = '../../assets/audio/how-to-use.mpeg';
+    link.click();
+}
 }
